d04d549c9b58fa9fab16743044a70f71c4330817
DenisSil <siliutindenis@yandex.ru>
2023-08-03 22:39:43 +0300

    start

--- lib/main.dart
+++ lib/main.dart
@@ -1,29 +1,123 @@
 import 'package:flutter/material.dart';
 
-void main() {
-  runApp(MyApp());
-}
+void main() => runApp(const NavigationBarApp());
 
-class MyApp extends StatelessWidget {
-  const MyApp({super.key});
+class NavigationBarApp extends StatelessWidget {
+  const NavigationBarApp({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'Flutter layout demo',
-      home: Scaffold(
-        appBar: AppBar(),
-        body: Center(
-          child:
-          TextButton(
-            style: ButtonStyle(
-              foregroundColor: MaterialStateProperty.all<Color>(Colors.blue),
-            ),
-            onPressed: () { },
-            child: Text('TextButton'),
+    return const MaterialApp(home: NavigationExample());
+  }
+}
+
+class NavigationExample extends StatefulWidget {
+  const NavigationExample({super.key});
+
+  @override
+  State<NavigationExample> createState() => _NavigationExampleState();
+}
+
+class _NavigationExampleState extends State<NavigationExample> {
+  int currentPageIndex = 0;
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      bottomNavigationBar: NavigationBar(
+
+        onDestinationSelected: (int index) {
+          setState(() {
+            currentPageIndex = index;
+          });
+        },
+        labelBehavior: NavigationDestinationLabelBehavior.onlyShowSelected,
+        selectedIndex: currentPageIndex,
+        destinations: const <Widget>[
+          NavigationDestination(
+            icon: Icon(Icons.list),
+            label: '',
           ),
-        ),
+          NavigationDestination(
+            icon: Icon(Icons.edit_note_sharp),
+            label: '',
+          ),
+        ],
       ),
+      body: <Widget>[
+        schedulePage(),
+        notesPage(),
+      ][currentPageIndex],
+    );
+  }
+}
+
+class schedulePage extends StatelessWidget{
+  @override
+  Widget build(BuildContext context) {
+    // TODO: implement build
+    return  MaterialApp(
+        home: Column(
+          children: [
+            Container(
+                margin: const EdgeInsets.all(20),
+                width: MediaQuery.of(context).size.width/3,
+                alignment: Alignment.center,
+                decoration: BoxDecoration(
+                    color: Colors.white,
+                    borderRadius: BorderRadius.circular(10),
+                    boxShadow: [
+                      BoxShadow(
+                        color: Colors.grey.withOpacity(0.5),
+                        spreadRadius: 2,
+                        blurRadius: 0,
+                        offset: const Offset(1, 2), // changes position of shadow
+                      ),
+                    ]
+                ),
+                child: InkWell(
+                    child: const Row(
+                        mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                        children: [
+                            Text('Понедельник 31.08',
+                                style: TextStyle(
+                                color: Colors.black,
+                                decoration: TextDecoration.none,
+                                fontSize: 20,
+                                )
+                            ),
+                            Icon(Icons.keyboard_arrow_down_sharp, size: 30,),
+                        ],
+                    ),
+                  onTap: (){
+                      print('нажал');
+                  },
+                )
+              ),
+          ],
+
+        )
+    );
+  }
+}
+
+class notesPage extends StatelessWidget{
+  @override
+  Widget build(BuildContext context) {
+    // TODO: implement build
+    return const MaterialApp(
+        home: Row(
+          mainAxisAlignment: MainAxisAlignment.spaceBetween,
+          children: [
+            Text('Понедельник 31.08',
+                style: TextStyle(
+                    color: Colors.black,
+                    decoration: TextDecoration.none,
+                    fontSize: 20,
+            )),
+            Icon(Icons.keyboard_arrow_down_sharp, size: 30,),
+          ],
+        )
     );
   }
 }
