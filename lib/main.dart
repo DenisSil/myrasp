2dd1b75e566e7bcb87e215755ec2805808fd41d5
DenisSil <siliutindenis@yandex.ru>
2023-08-04 11:35:44 +0300

    added subject card

--- lib/main.dart
+++ lib/main.dart
@@ -1,24 +1,26 @@
+import 'dart:ui';
+
 import 'package:flutter/material.dart';
 
-void main() => runApp(const NavigationBarApp());
+void main() => runApp(const MyRasp());
 
-class NavigationBarApp extends StatelessWidget {
-  const NavigationBarApp({super.key});
+class MyRasp extends StatelessWidget {
+  const MyRasp({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return const MaterialApp(home: NavigationExample());
+    return const MaterialApp(home: MyApp());
   }
 }
 
-class NavigationExample extends StatefulWidget {
-  const NavigationExample({super.key});
+class MyApp extends StatefulWidget {
+  const MyApp({super.key});
 
   @override
-  State<NavigationExample> createState() => _NavigationExampleState();
+  State<MyApp> createState() => MyAppState();
 }
 
-class _NavigationExampleState extends State<NavigationExample> {
+class MyAppState extends State<MyApp> {
   int currentPageIndex = 0;
 
   @override
@@ -44,20 +46,30 @@ class _NavigationExampleState extends State<NavigationExample> {
           ),
         ],
       ),
-      body: <Widget>[
+      body: Center( child:<Widget>[
         schedulePage(),
         notesPage(),
       ][currentPageIndex],
+      )
     );
   }
 }
 
-class schedulePage extends StatelessWidget{
+class schedulePage extends StatefulWidget{
+
+  @override
+  State<schedulePage> createState() => _schedulePageState();
+}
+
+class _schedulePageState extends State<schedulePage> {
+
+  bool clickCard = false;
+
   @override
   Widget build(BuildContext context) {
+
     // TODO: implement build
-    return  MaterialApp(
-        home: Column(
+    return Column(
           children: [
             Container(
                 margin: const EdgeInsets.all(20),
@@ -65,39 +77,45 @@ class schedulePage extends StatelessWidget{
                 alignment: Alignment.center,
                 decoration: BoxDecoration(
                     color: Colors.white,
-                    borderRadius: BorderRadius.circular(10),
+                    borderRadius: BorderRadius.circular(8),
                     boxShadow: [
                       BoxShadow(
-                        color: Colors.grey.withOpacity(0.5),
-                        spreadRadius: 2,
-                        blurRadius: 0,
-                        offset: const Offset(1, 2), // changes position of shadow
+                        color: Colors.black.withOpacity(0.25),
+                        spreadRadius: 0,
+                        blurRadius: 4,
+                        offset: const Offset(0, 4), // changes position of shadow
                       ),
                     ]
                 ),
-                child: InkWell(
-                    child: const Row(
-                        mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                        children: [
-                            Text('Понедельник 31.08',
-                                style: TextStyle(
-                                color: Colors.black,
-                                decoration: TextDecoration.none,
-                                fontSize: 20,
-                                )
-                            ),
-                            Icon(Icons.keyboard_arrow_down_sharp, size: 30,),
-                        ],
+                child: Column(
+                  children: [
+                    InkWell(
+                        child:  Row(
+                            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                            children: [
+                                const Text('Понедельник 31.08',
+                                    style: TextStyle(
+                                    color: Colors.black,
+                                    decoration: TextDecoration.none,
+                                    fontSize: 20,
+                                    )
+                                ),
+                              if (clickCard)  const Icon(Icons.keyboard_arrow_up_outlined, size: 30,)
+                              else const Icon(Icons.keyboard_arrow_down_outlined, size: 30,)
+                            ],
+                        ),
+                        onTap: (){
+                            setState(() {
+                              clickCard = clickCard == true? false: true;
+                            });
+                      },
                     ),
-                  onTap: (){
-                      print('нажал');
-                  },
-                )
-              ),
+                  if (clickCard) const SubjectCard()
+                  ],
+                ),
+            ),
           ],
-
-        )
-    );
+        );
   }
 }
 
@@ -121,3 +139,63 @@ class notesPage extends StatelessWidget{
     );
   }
 }
+
+class SubjectCard extends StatelessWidget {
+  const SubjectCard({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      margin: const EdgeInsets.all(20),
+      padding: const EdgeInsets.all(10),
+      decoration: BoxDecoration(
+        color: Colors.white,
+        borderRadius: BorderRadius.circular(4),
+        boxShadow: [
+          BoxShadow(
+            color: Colors.black.withOpacity(0.25),
+            spreadRadius: 0,
+            blurRadius: 4,
+            offset: const Offset(0, 4), // changes position of shadow
+          ),
+        ]
+      ),
+      child:  Row(
+        children: [
+          Column(
+            children: [
+              const Text('14:15'),
+              Container(
+                width: 30,
+                color: Colors.black,
+                height: 1,
+                margin: const EdgeInsets.symmetric(vertical: 5, horizontal: 0),
+              ),
+              const Text('15:50'),
+              const Padding(padding:
+                EdgeInsets.only(top: 10),
+                child : Text('8-421')
+              )
+            ],
+          ),
+          Container(
+            margin: const EdgeInsets.only(left: 20),
+            child: const Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                Text('пр. Аналитическая\nгеометрия'),
+                Padding(
+                  padding: EdgeInsets.only(top: 5.0),
+                  child: Text('ст.пр.Артамонова Елена\nАлександровна'),
+                ),
+              ],
+            ),
+          )
+        ],
+      ),
+    );
+  }
+}
+
+
+
